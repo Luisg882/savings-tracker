2333642ea02c9c9ccc6eb9a786db1ae17dd4e0dd
Geras882 <luisgoncalvesfigueira@gmail.com>
2024-07-01 19:44:25 +0100

    display the saving pots with pagination in the profile.html

--- saving/views.py
+++ saving/views.py
@@ -1,13 +1,26 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Profile
+
 
 def home_view(request):
     if request.user.is_authenticated:
         return redirect('profile')
     return render(request, 'account/index.html')
 
+
 @login_required
 def profile_view(request):
     profile = Profile.objects.get(user=request.user)
-    return render(request, 'account/profile.html', {'profile': profile})
+    saving_pots_list = profile.saving_pots.all()
+
+    paginator = Paginator(saving_pots_list, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request, 'account/profile.html', 
+        {'profile': profile,
+         'page_obj': page_obj}
+        )
