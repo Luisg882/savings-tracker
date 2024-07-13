2ab0527e3376d6da321b94805d08ab08a090b1c6
Geras882 <luisgoncalvesfigueira@gmail.com>
2024-07-13 12:26:13 +0100

    Fix migration issue that didn't allow user to create new saving pot

--- saving/models.py
+++ saving/models.py
@@ -26,7 +26,7 @@ class SavingPot(models.Model):
         User, on_delete=models.CASCADE, related_name="user"
     )
     name = models.CharField(max_length=200, unique=True)
-    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    balance = models.DecimalField(max_digits=10, decimal_places=3, default=0.00)
     created_on = models.DateTimeField(auto_now_add=True)
 
     class Meta:
